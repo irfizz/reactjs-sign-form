7221695106d55254fa0bd3da0b984038851b7d65
irfizz <irfan.izzat.azman.88@gmail.com>
2024-05-22 21:31:50 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoginSignup from "./Components/Log in Sign Up/LoginSignup";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
